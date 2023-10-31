b167b0d459de6694efc12c5f010187e00da1a44a
Senerro <shadowpi1999@gmail.com>
2023-10-31 17:22:05 +0300

    All Task2 reqierments is complited

--- Task2/src/Farm/Farm.java
+++ Task2/src/Farm/Farm.java
@@ -5,6 +5,18 @@ import ReqularClasses.*;
 public class Farm {
     private static int purchase;
     public FarmingList farmingList = new FarmingList();
+    public Farm()
+    {
+        PlantAppleTree (1, "golden");
+        BullPurchase(2, 15, 950);
+        PlantCabbage ( 3,  "Chineese");
+        ChickenPurchase(2, 2, true);
+        CowPurchase(5, 1);
+        HorsePurchase(10, 7, 999);
+        PlantLemonTree(10);
+        PlantPotato(5, "Aspia");
+        SheepPurchase(5, 4);
+    }
     public void PlantAppleTree(int count, String sort)
     {
 
@@ -22,7 +34,7 @@ public class Farm {
         for (int i = 0; i < count; i++)
         {
             var acquisition = new Bull();
-            acquisition.SetName("bull number " + i + " from procurement " + purchase);
+            acquisition.SetName("Bull number " + i + " from procurement " + purchase);
             acquisition.SetTonage(tonnage);
             acquisition.SetAge(age);
             farmingList.AddWorkCastles(acquisition);
@@ -37,39 +49,38 @@ public class Farm {
             farmingList.AddVegetable(acquisition);
         }
     }
-    public void ChickenPurchase (int count, int age, float tonnage)
+    public void ChickenPurchase (int count, int age, boolean sex)
     {
         for (int i = 0; i < count; i++)
         {
             var acquisition = new Chicken();
-            acquisition.SetName("chicken number " + i + " from procurement " + purchase);
+            acquisition.SetName("Chicken number " + i + " from procurement " + purchase);
 
             acquisition.SetAge(age);
             acquisition.SetAge(i);
-            acquisition.SetChickenSex(true);
+            acquisition.SetChickenSex(sex);
             farmingList.AddRawCattle(acquisition);
         }
     }
-    public void CowPurchase (int count, int age, float tonnage)
+    public void CowPurchase (int count, int age)
     {
         for (int i = 0; i < count; i++)
         {
             var acquisition = new Cow();
-            acquisition.SetName("cow number " + i + " from procurement " + purchase);
+            acquisition.SetName("Cow number " + i + " from procurement " + purchase);
             acquisition.SetAge(age);
             acquisition.SetColor(0);//black
             farmingList.AddRawCattle(acquisition);
         }
     }
 
-
-    public void HorsePurchase (int count, int age, float tonnage)
+    public void HorsePurchase (int count, int age, int endurance)
     {
         for (int i = 0; i < count; i++)
         {
             var acquisition = new Horse();
             acquisition.SetName("horse number " + i + " from procurement " + purchase);
-            acquisition.SetMaxEndurance(500);
+            acquisition.SetMaxEndurance(endurance);
             acquisition.SetAge(age);
             farmingList.AddWorkCastles(acquisition);
         }
@@ -80,12 +91,21 @@ public class Farm {
         for (int i = 0; i < count; i++)
         {
             var acquisition = new LemonTree();
-            acquisition.SetName("Apple tree number " + i + " from procurement " + purchase);
+            acquisition.SetName("Lemon tree number " + i + " from procurement " + purchase);
             acquisition.SetCropYield(50);
             acquisition.SetAcidLevel(4);//very acid
             farmingList.AddFruit(acquisition);
         }
 
+    }
+    public void PigPurchase (int count, int age)
+    {
+        for (int i = 0; i < count; i++) {
+            var acquisition = new Pig();
+            acquisition.SetName("Pig number " + i + " from procurement " + purchase);
+            acquisition.SetAge(age);
+            farmingList.AddRawCattle(acquisition);
+        }
     }
     public void PlantPotato (int count, String sort)
     {
@@ -99,13 +119,18 @@ public class Farm {
             farmingList.AddVegetable(acquisition);
         }
     }
-    public void SheepPurchase (int count, int age, float tonnage)
+    public void SheepPurchase (int count, int age)
     {
         for (int i = 0; i < count; i++) {
             var acquisition = new Sheep();
-            acquisition.SetName("chicken number " + i + " from procurement " + purchase);
-            acquisition.SetAge(i);
+            acquisition.SetName("Sheep number " + i + " from procurement " + purchase);
+            acquisition.SetAge(age);
             farmingList.AddRawCattle(acquisition);
         }
+    }
+    public static void main(String[] args)
+    {
+        var obj = new Farm();
+       System.out.println("All is working");
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,15 +1,10 @@
 import ReqularClasses.Chicken;
 
 public class Main {
-    public static void main(String[] args)
+
+  /*  public static void main(String[] args)
     {
         var obj = new Chicken(false);
         System.out.println(obj.GetName());
-//
-//
-//
-//
-//
-//— Создайте отдельный класс с main(), который будет создавать экземпляры объектов реализованных классов.
-    }
+    }*/
 }
